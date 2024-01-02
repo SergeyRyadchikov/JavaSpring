04e99b1c3c9b0ff23fd1d3db61a9df006e8802a6
ryadchikov.sa <ryadchikov.sa@gmail.com>
2024-01-02 20:29:57 +0600

    Added lesson1

---
+++ lesson1/src/main/java/gb/spring/App.java
@@ -0,0 +1,17 @@
+package gb.spring;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        String New;
+        Person person = new Person("Igor");
+        person.setAge(18);
+        person.setPhone("8902347846532");
+        System.out.printf("%s - %s\n%s", person.getName(), person.getAge(), person);
+    }
+}
---
+++ lesson1/src/test/java/gb/spring/AppTest.java
@@ -0,0 +1,24 @@
+package gb.spring;
+
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+
+
+
+public class AppTest{
+
+
+    @Test
+    public void firstTest(){
+        Assertions.assertEquals(5, 5);
+    }
+
+    @Test
+    public void secondTest(){
+        Assertions.assertEquals(1, 1);
+    }
+
+}
